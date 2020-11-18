8e5671f3d796e5da82aa2f17d5040c9210b64e88
saniag081 <sgonzalez@booksandbooksdigital.com.co>
2020-11-18 16:31:02 -0500

    feat (Modal): se agrega componente

--- src/components/atoms/icon/index.js
+++ src/components/atoms/icon/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 import css from './Icon.module.css'
 
-export const Icon = ({ nameIcon }) => {
-  return <i className={css.Icon}>{nameIcon}</i>
+export const Icon = ({ nameIcon, ...args }) => {
+  return (
+    <i className={css.Icon} {...args}>
+      {nameIcon}
+    </i>
+  )
 }
