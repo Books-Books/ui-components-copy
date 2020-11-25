317aaf3ffc19a0fdafdb6a3b23dd7265dde88fb1
saniag081 <sgonzalez@booksandbooksdigital.com.co>
2020-11-25 16:56:23 -0500

    fix (Accordion): ajustes

--- src/components/atoms/DropableItem/index.js
+++ src/components/atoms/DropableItem/index.js
@@ -3,16 +3,17 @@ import { Icon } from '../icon/index'
 import PropTypes from 'prop-types'
 import css from './DropableItem.module.css'
 
-export function DropableItem({ item, index, ...props }) {
+export function AccordionItem({ item, index, stateIcon, ...props }) {
   return (
     <Fragment>
       <div
         className={css['c-collapsible-container-header']}
-        data-element={`body${index}`}
+        // data-element={`body${index}`}
         {...props}
       >
         <Icon nameIcon={item.dataIcon} />
         <p> {item.title}</p>
+        <Icon nameIcon={stateIcon} />
       </div>
       <div className={css['c-collapsible-container-body']} id={`body${index}`}>
         <p>{item.text}</p>
@@ -21,7 +22,12 @@ export function DropableItem({ item, index, ...props }) {
   )
 }
 
-DropableItem.propTypes = {
+AccordionItem.propTypes = {
   item: PropTypes.object,
+  stateIcon: PropTypes.string,
   index: PropTypes.number
+}
+
+AccordionItem.defaultProps = {
+  stateIcon: 'expand_more'
 }
