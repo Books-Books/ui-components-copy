d798fe96fdecef1088a5a950869aa3c8adb75497
saniag081 <sgonzalez@booksandbooksdigital.com.co>
2020-11-26 16:59:07 -0500

    feat (Graphics): se agrega componente

---
+++ src/components/molecules/Graphics/Graphics.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Graphics } from './index'
+
+export default {
+  title: 'Molecules/Graphics',
+  componente: Graphics
+}
+
+const Template = (args) => <Graphics {...args} />
+
+export const FristGraphics = Template.bind({})
