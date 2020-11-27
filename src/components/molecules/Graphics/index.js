33931ae718bcf7c7b480bbc6d114b1d67fd27301
saniag081 <sgonzalez@booksandbooksdigital.com.co>
2020-11-27 16:54:01 -0500

    feat (Graphics): se agregan argumentos y historias

--- src/components/molecules/Graphics/index.js
+++ src/components/molecules/Graphics/index.js
@@ -1,33 +1,27 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 import { Chart } from 'react-google-charts'
 
-function Graphics() {
+function Graphics({ width, height, chartType, data, options, props }) {
   return (
     <Chart
-      width={600}
-      height={500}
-      chartType='ColumnChart'
+      width={width}
+      height={height}
+      chartType={chartType}
       loader={<div>Cargando...</div>}
-      data={[
-        ['City', '2010 Population', '2000 Population'],
-        ['New York City, NY', 8175000, 8008000],
-        ['Los Angeles', 3792000, 3694000],
-        ['Chicago, IL', 2695000, 2896000]
-      ]}
-      options={{
-        title: 'Population of Largest U.S Cities',
-        chartArea: { width: '50%' },
-        hAxis: {
-          title: 'Total population',
-          minValue: 0
-        },
-        vAxis: {
-          title: 'City'
-        }
-      }}
+      data={data}
+      options={options}
+      {...props}
       legendToggle
     />
   )
 }
 
 export { Graphics }
+Graphics.propTypes = {
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  chartType: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
+  options: PropTypes.object.isRequired
+}
