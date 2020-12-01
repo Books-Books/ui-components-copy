e39daf1d50353d42185873bd78d1b4790159da72
saniag081 <sgonzalez@booksandbooksdigital.com.co>
2020-12-01 08:26:09 -0500

    feat (Graphinncs, Table): se agregan componentes a example

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,6 +1,8 @@
 export * from './AsideNav'
 export * from './Card'
 export * from './Dropable'
+export * from './Graphics'
 export * from './Modal'
+export * from './Table'
 export * from './Tabs'
 
