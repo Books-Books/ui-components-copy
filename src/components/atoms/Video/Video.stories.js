231f83c7a8abcf10683533eb413d442db161779f
saniag081 <sgonzalez@booksandbooksdigital.com.co>
2020-12-04 16:50:27 -0500

    feat (Video): se agrega estilo

--- src/components/atoms/Video/Video.stories.js
+++ src/components/atoms/Video/Video.stories.js
@@ -13,5 +13,5 @@ export const baseVideo = Template.bind({})
 baseVideo.args = {
   url:
     'https://storage.googleapis.com/cedoc360extencion/a_english_multimedia/a1/module_1/videos/a1m1a-ae_vid_1.mp4',
-  width: '400px'
+  width: '600px'
 }
