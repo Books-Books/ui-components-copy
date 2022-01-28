34408d0e6a62c175c67d3098ac131cdf89151516
Cristian Díaz <maquetador1@booksandbooksdigital.com.co>
2022-01-28 11:43:12 -0500

    Cambio de semántica a row

--- src/components/utilities/layout/Row/index.js
+++ src/components/utilities/layout/Row/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import css from './Row.module.css'
 
 export const Row = (props) => {
-  return <section className={css.row} {...props} />
+  return <div className={css.row} {...props} />
 }
