55e6369d11439ad2d307738f0df12c21088cd500
Cristian Díaz <maquetador1@booksandbooksdigital.com.co>
2022-01-31 14:19:26 -0500

    Arreglo de maquetación de componente Dropdown

--- src/components/atoms/Dropdown/Dropdown.stories.js
+++ src/components/atoms/Dropdown/Dropdown.stories.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Row } from '../../utilities'
+import React, { Fragment } from 'react'
 import { Dropdown } from './'
 
 export default {
@@ -8,16 +7,31 @@ export default {
 }
 
 export const DropdownDefault = (args) => {
+  const playButton = (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='48'
+      height='48'
+      viewBox='0 0 24 24'
+      aria-hidden='true'
+      className='svg-icon'
+      focusable='false'
+    >
+      <path fill='none' d='M0 0h24v24H0z'></path>
+      <path d='M8 5v14l11-7z'></path>
+    </svg>
+  )
+
   return (
-    <Row flex='true' justify-content='around'>
-      <Dropdown>
+    <Fragment>
+      <Dropdown icon={playButton}>
         <p>item_1</p>
         <p>item_2</p>
       </Dropdown>
-      <Dropdown>
+      <Dropdown icon=''>
         <p>item_3</p>
         <p>item_4</p>
       </Dropdown>
-    </Row>
+    </Fragment>
   )
 }
--- src/components/atoms/Dropdown/index.js
+++ src/components/atoms/Dropdown/index.js
@@ -1,44 +1,109 @@
 import React, { useState } from 'react'
-import { Icon } from '../icon'
+import { Button } from '../Button'
 import css from './Dropdown.module.css'
 
-export const Dropdown = ({ icon, children: childsElem, label, addClass }) => {
+export const Dropdown = ({
+  icon = '',
+  children,
+  hasAriaLabel = false,
+  label,
+  addClass
+}) => {
   const [Expanded, SetExpanded] = useState(false)
 
-  function HandleChange() {
-    SetExpanded(!Expanded)
+  const toggleMenu = () => {
+    Expanded ? SetExpanded(false) : SetExpanded(true)
   }
+
+  const closeMenu = () => {
+    SetExpanded(false)
+  }
+
+  const closeMenuOnEsc = (e) => {
+    if ((e.keyCode || e.which) === 27) SetExpanded(false)
+  }
+
   return (
-    <div
-      className={`${css.DropdownCont} ${addClass} `}
-      onMouseOver={HandleChange}
-      onMouseOut={HandleChange}
-      tabIndex='1'
-    >
-      {icon && <Icon nameIcon={icon} />}
-      <ul>
-        <li className={css.navItem}>
-          <span
-            className={css.navLink}
-            id='navbarDropdown'
-            role='button'
-            data-toggle='dropdown'
-            aria-haspopup='true'
-            aria-expanded={Expanded}
-          >
-            {label}
-          </span>
-          <div className={css.dropdownMenu} aria-labelledby='navbarDropdown'>
-            {React.Children.map(childsElem, (elem, idx) => (
-              <div className={css['dropdownMenu-item']} key={idx}>
-                {elem}
-              </div>
-            ))}
-          </div>
-        </li>
+    <div className={css['dropdownContainer']}>
+      <Button
+        hasAriaLabel={hasAriaLabel}
+        aria-expanded={Expanded}
+        className={`${css.DropdownCont} ${addClass} `}
+        onClick={toggleMenu}
+        onBlur={closeMenu}
+        onKeyDown={closeMenuOnEsc}
+      >
+        {icon}
+        {label}
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='48'
+          height='48'
+          viewBox='0 0 24 24'
+          focusable='false'
+          aria-hidden='true'
+          className={css['dropdownArrow']}
+        >
+          <path fill='none' d='M0 0h24v24H0z'></path>
+          <path d='M7 10l5 5 5-5z'></path>
+        </svg>
+      </Button>
+      {/* <button
+        aria-expanded={Expanded}
+        className={`${css.DropdownCont} ${addClass} `}
+        onClick={toggleMenu}
+        onBlur={closeMenu}
+        onKeyDown={closeMenuOnEsc}
+      >
+        {icon}
+        {label}
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='48'
+          height='48'
+          viewBox='0 0 24 24'
+          focusable='false'
+          aria-hidden='true'
+          className={css['dropdownArrow']}
+        >
+          <path fill='none' d='M0 0h24v24H0z'></path>
+          <path d='M7 10l5 5 5-5z'></path>
+        </svg>
+      </button> */}
+      <ul role='list' className={css.dropdownMenu}>
+        {children}
       </ul>
-      <Icon nameIcon={Expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'} />
     </div>
+    // <div
+    //   className={`${css.DropdownCont} ${addClass} `}
+    //   onMouseOver={HandleChange}
+    //   onMouseOut={HandleChange}
+    //   tabIndex='1'
+    // >
+    //   {icon && <Icon nameIcon={icon} />}
+    //   <ul>
+    //     <li className={css.navItem}>
+    //       <span
+    //         className={css.navLink}
+    //         id='navbarDropdown'
+    //         role='button'
+    //         data-toggle='dropdown'
+    //         aria-haspopup='true'
+    //         aria-expanded={Expanded}
+    //       >
+    //         {label}
+    //       </span>
+    //       <div className={css.dropdownMenu} aria-labelledby='navbarDropdown'>
+    //         {React.Children.map(childsElem, (elem, idx) => (
+    //           <div className={css['dropdownMenu-item']} key={idx}>
+    //             {elem}
+    //           </div>
+    //         ))}
+    //       </div>
+    //     </li>
+    //   </ul>
+    //   <Icon nameIcon={Expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'} />
+    // </div>
   )
 }
 
