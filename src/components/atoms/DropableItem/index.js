1327bd238bab49e182f299da179c40a706bbd808
Cristian Díaz <maquetador1@booksandbooksdigital.com.co>
2022-02-01 07:36:06 -0500

    Adición de configuraciones de accesibilidad a DropableItem

--- src/components/atoms/DropableItem/index.js
+++ src/components/atoms/DropableItem/index.js
@@ -6,15 +6,16 @@ import css from './DropableItem.module.css'
 export function AccordionItem({ item, index, stateIcon, addClass, ...props }) {
   return (
     <Fragment>
-      <div
+      <button
         className={`${css['c-collapsible-container-header']} ${addClass}`}
+        aria-expanded={false}
         // data-element={`body${index}`}
         {...props}
       >
         <Icon nameIcon={item.dataIcon} />
         <p> {item.title}</p>
         <Icon nameIcon={stateIcon} />
-      </div>
+      </button>
       <div className={css['c-collapsible-container-body']} id={`body${index}`}>
         <p>{item.text}</p>
       </div>
