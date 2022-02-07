7e1e98d597c1f1ef0c85e4800b1e5499dfe8723f
Cristian Díaz <maquetador1@booksandbooksdigital.com.co>
2022-02-07 10:13:41 -0500

    AMejora de accesibilidad de componente Carrousel

--- src/components/atoms/panel/index.js
+++ src/components/atoms/panel/index.js
@@ -2,15 +2,24 @@ import React from 'react'
 import css from './Panel.module.css'
 
 export const TabPanel = (props) => {
-  const { children, value, index, addClass, ...other } = props
+  const {
+    children,
+    value,
+    index,
+    addClass,
+    roleDescription = 'slide',
+    ...other
+  } = props
 
   return (
     <div
       className={`${css.Panel} ${addClass}`}
-      role='tabpanel'
+      role='group'
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
+      aria-roledescription={roleDescription}
       aria-labelledby={`simple-tab-${index}`}
+      tabIndex='0'
       {...other}
     >
       {value === index && <div>{children}</div>}
