d830a3b3fc705395eb26b4055593b3b3ef2e4eb3
Cristian Díaz <maquetador1@booksandbooksdigital.com.co>
2022-02-16 15:55:11 -0500

    ACambio de estilos a componente Video

--- src/components/atoms/Video/Video.stories.js
+++ src/components/atoms/Video/Video.stories.js
@@ -11,8 +11,9 @@ const Template = (args) => <Video {...args} />
 export const baseVideo = Template.bind({})
 
 baseVideo.args = {
-  url:
-    'https://storage.googleapis.com/cedoc360extencion/a_english_multimedia/a1/module_1/videos/a1m1a-ae_vid_1.mp4',
+  url: 'https://storage.googleapis.com/cedoc360extencion/a_english_multimedia/a1/module_1/videos/a1m1a-ae_vid_1.mp4',
   width: '600px',
-  addClass: 'prb'
+  addClass: 'prb',
+  title: 'Video 1',
+  content: 'Video de prueba'
 }
