8941fdddbf5b12f6a8c9f4f96d6dfa0080b6c6ec
Cristian Díaz <maquetador1@booksandbooksdigital.com.co>
2022-02-28 09:30:24 -0500

    AdiciÂón de ModalTest

--- src/components/atoms/ModalButton/index.js
+++ src/components/atoms/ModalButton/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import modalCss from '../../molecules/ModalTest/Modal.module.css'
 import { Button, Image } from '../index'
 import css from './ModalButton.module.css'
 
@@ -15,18 +16,24 @@ export const ModalButton = ({
 }) => {
   const openModal = function (id) {
     const modal = document.getElementById(`modal${id}`)
+    const modalOverlay = document.getElementById(`modalOverlay${id}`)
+
     modal.hidden = false
     modal.focus()
+
+    modal.classList.add(modalCss['modal--active'])
+    modalOverlay.classList.add(modalCss['overlay--active'])
   }
 
   return (
     <Fragment>
       {hasImage ? (
         <button
-          className={css['.modal-button-image']}
+          className={css['modal-button-image']}
           aria-labelledby={`modalDescription${id}`}
           aria-description='Abrir modal'
           onClick={() => openModal(id)}
+          id={`openModal${id}`}
         >
           <span className='sr-only' id={`modalDescription${id}`}>
             {label}
