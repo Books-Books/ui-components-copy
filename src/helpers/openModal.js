eda8f5718a66355f3bd2a551989b81fc86642d2b
Cristian Díaz <maquetador1@booksandbooksdigital.com.co>
2022-03-04 08:59:34 -0500

    AdiciÂon de keyboard trap a modal

--- src/helpers/openModal.js
+++ src/helpers/openModal.js
@@ -5,15 +5,33 @@ const createKeyboardTrap = function (id) {
 
   const focusableElements = [
     ...modal.querySelectorAll(
-      'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]'
+      'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed [tabindex="0"], [contenteditable]'
     )
   ]
   const firstFocusableElement = focusableElements[0]
   const lastFocusableElement = focusableElements[focusableElements.length - 1]
 
-  console.log(focusableElements)
-  console.log(firstFocusableElement)
-  console.log(lastFocusableElement)
+  // console.log(focusableElements)
+  // console.log(firstFocusableElement)
+  // console.log(lastFocusableElement)
+
+  function trapTabKey(e) {
+    if (e.keyCode === 9) {
+      if (e.shiftKey) {
+        if (document.activeElement === firstFocusableElement) {
+          e.preventDefault
+          lastFocusableElement.focus()
+        }
+      } else {
+        if (document.activeElement === lastFocusableElement) {
+          e.preventDefault
+          firstFocusableElement.focus()
+        }
+      }
+    }
+  }
+
+  trapTabKey(e)
 }
 
 export const openModal = function (id) {
